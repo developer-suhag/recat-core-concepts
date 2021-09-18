4eaa20a8c9d905fcc77a1d1cd323e07066db4ed5
developer-suhag <developersuhag@gmail.com>
2021-09-18 08:46:26 +0600

    jsx

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
+      <Suhag></Suhag>
     </div>
   );
 }
+
+function Person() {
+  return (
+    <div className="person">
+      <h2>Shakib Al Hasan</h2>
+      <p>Profession: Cricketer</p>
+    </div>
+  );
+}
+
+const Suhag = () => {
+  const suhag = {
+    backgroundColor: "tomato",
+    border: "3px solid violet",
+    borderRadius: "20px",
+    color: "#fff",
+    margin: "20px",
+  };
+  return (
+    <div style={suhag}>
+      <h2>Name: Suhag</h2>
+      <p>Profession: Programmer</p>
+    </div>
+  );
+};
 
 export default App;
